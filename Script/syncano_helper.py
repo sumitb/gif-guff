405d517eec01228912716f08c18a0204719cfa59
Sumit Bindal <sbindal@cs.stonybrook.edu>
2015-03-08 06:46:29 -0400

    getPost functionality

--- Script/syncano_helper.py
+++ Script/syncano_helper.py
@@ -56,17 +56,30 @@ def delete_comment(folder_name, comment):
 	if idx != 0:
 		syncano.data_delete(6182, collection_id=18729, collection_key='trackSubmit', data_ids=idx,
 			state='All', folders=folder_name, filter=None, by_user=None, limit=100, message_id=None)
-	
+
+def get_post():
+	INSTANCE = "holy-surf-639401"
+        API_KEY = "bd6e4f117fe7f6c1db89a9bc626c06c92ec6304d"
+	syncano = SyncanoApi(INSTANCE, api_key=API_KEY)
+	folder=syncano.folder_get(6182, collection_id=18729, collection_key='trackSubmit', message_id=None)
+	count = len(folder['data']['folder'])
+	folder_name=[]	
+	for i in range(1, count):
+		 folder_name.append(folder['data']['folder'][i]['name'])
+
+	print folder_name
 
 if __name__ == "__main__":
-
-	if sys.argv[1] == "addPost":
-		add_post(sys.argv[2])
-	elif sys.argv[1] == "getComments":
-		get_comments(sys.argv[2])
-	elif sys.argv[1] == "deletePost":
-		delete_post(sys.argv[2])
-	elif sys.argv[1] == "addComments":
-		add_comment(sys.argv[2], sys.argv[3])
-	elif sys.argv[1] == "deleteComments":
-		delete_comment(sys.argv[2], sys.argv[3])
+	
+        if sys.argv[1] == "addPost":
+                add_post(sys.argv[2])
+        elif sys.argv[1] == "getComments":
+                get_comments(sys.argv[2])
+        elif sys.argv[1] == "deletePost":
+                delete_post(sys.argv[2])
+        elif sys.argv[1] == "addComments":
+                add_comment(sys.argv[2], sys.argv[3])
+        elif sys.argv[1] == "deleteComments":
+                delete_comment(sys.argv[2], sys.argv[3])
+        elif sys.argv[1] == "getPost":
+                get_post()
